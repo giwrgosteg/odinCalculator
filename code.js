86011dfd348faf57c37b122e97f9e78ab6ad85b9
George <giwrgosteg@gmail.com>
2024-08-08 15:10:05 +0300

    Made the decimal and the rounding to 1 decimal methods, also made the "Error" print when dividing by 0.

--- code.js
+++ code.js
@@ -10,11 +10,13 @@ const decimal = document.getElementById("decimal");
 
 let mathArray = [0, '', 0];
 let index = 0;
+let decBool = false;
 
 function clearFunction() {
-    displayUpdate('0', 1);
-    mathArray = ['0', '', '0'];
+    displayUpdate("0", 1);
+    mathArray = ["0", "", "0"];
     index = 0;
+    decBool = false;
 }
 
 function displayUpdate(text, num) {
@@ -22,16 +24,23 @@ function displayUpdate(text, num) {
         result.innerText = text;
     }
 
-    if (result.innerText == '0' && num == 0){
+    if (result.innerText == "0" && num == 0){
         result.innerText = text; 
     }
-    else if (result.innerText != '0' && num == 0) {
+    else if (result.innerText != "0" && num == 0) {
         result.innerText += text;
     }
 }
 
+function decimalFunction() {
+    if (decBool == false) {
+        result.innerText += "."; 
+        mathArray[index] += ".";
+        decBool = true;
+    }
+}
+
 function numberFunction() {
-    console.log(this.innerText);
     if (mathArray[index] == 0) {
         mathArray[index] = this.innerText;
     }
@@ -42,55 +51,66 @@ function numberFunction() {
     displayUpdate(this.innerText, 0);
 }
 
-function operate() {
-    if (index == 2) {
-        equalFunction();
+function operationsFunction() {
+    if (index == 2 && mathArray[2] != "0") {
+        operate();
     }
 
-    mathArray[1] = this.innerText;
-    index = 2;
-    displayUpdate(this.innerText, 0);
+    if (mathArray[2] == '0') {
+        displayUpdate(mathArray[0] + this.innerText, 1);
+        mathArray[1] = this.innerText;
+        index = 2;
+    }
+    else {
+        mathArray[1] = this.innerText;
+        index = 2;
+        displayUpdate(this.innerText, 0);
+    }
 }
 
-function equalFunction() {
-    let result = '0';
+function operate() {
+    let result = "0";
 
     if (index == 2) {
         let first = Number(mathArray[0]);
         let second = Number(mathArray[2]);
-        console.log(mathArray);
         if  (mathArray[1] == "+") {
-            result = String(first + second);
+            result = String(Math.round((first + second) * 10) / 10);
             result.innerText = result;
         }
         else if (mathArray[1] == "-") {
-            result = String(first - second);
+            result = String(Math.round((first - second) * 10) / 10);
             result.innerText = result;
         }
-        else if (mathArray[1] == "*") {
-            result = String(first * second)
+        else if (mathArray[1] == "x") {
+            result = String(Math.round((first * second) * 10) / 10)
             result.innerText = result;
         }
         else if (mathArray[1] == "/") {
-            result = String(first / second)
+            if (second == 0) {
+                displayUpdate("Error", 1);
+                return;
+            }
+            result = String(Math.round((first / second) * 10) / 10)
             result.innerText = result;
         }
     }
-    mathArray = [result, '', '0'];
+    mathArray = [result, "", "0"];
     displayUpdate(result, 1);
     index = 0;
+    decBool = false;
 }
 
 for (let i = 0; i < operations.length; i++){
-    operations[i].addEventListener("click", operate);
+    operations[i].addEventListener("click", operationsFunction);
 }
 
 for (let i = 0; i < numbers.length; i++){
     numbers[i].addEventListener("click", numberFunction);
 }
 
-equal.addEventListener("click", equalFunction);
+equal.addEventListener("click", operate);
 clear.addEventListener("click", clearFunction);
-decimal.addEventListener("click", operate);
+decimal.addEventListener("click", decimalFunction);
 
 
